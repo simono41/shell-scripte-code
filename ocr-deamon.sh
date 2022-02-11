e1d64e8bbcb1e3c709107911faf5d22356643f55
Simon Rieger <srieger@spectreos.de>
2022-02-11 14:41:22 +0100

    while read bugfix

--- ocr-deamon.sh
+++ ocr-deamon.sh
@@ -2,6 +2,11 @@ set -x
 
 DOKUMENT_DIR=/home/simono41/data1/Dokumente
 
-for file in $(find ${DOKUMENT_DIR} -path ${DOKUMENT_DIR}/.stversions -prune -o -name "*.pdf" ! -name '*-ocr.pdf' -print); do
-	ocrmypdf -l deu --force-ocr --jbig2-lossy ${file} ${file%.*}-ocr.pdf
-done 
+find ${DOKUMENT_DIR} -path ${DOKUMENT_DIR}/.stversions -prune -type f -o -name "*.pdf" ! -name '*-ocr.pdf' -print | while read file; do
+  if ! [ -f "${file%.*}-ocr.pdf" ]; then
+    ocrmypdf -l deu --force-ocr --jbig2-lossy "${file}" "${file%.*}-ocr.pdf"
+    sleep 1
+  else
+    echo "Datei wurde bereits verarbeitet"
+  fi
+done
