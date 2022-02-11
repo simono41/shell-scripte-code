f52bd317a1f74399125c85379684f89bc15c2f25
Simon Rieger <srieger@spectreos.de>
2022-02-12 00:08:06 +0100

    remove force-ocr for only scan documents

--- ocr-deamon.sh
+++ ocr-deamon.sh
@@ -4,7 +4,7 @@ DOKUMENT_DIR=/home/simono41/data1/Dokumente
 
 find ${DOKUMENT_DIR} -path ${DOKUMENT_DIR}/.stversions -prune -type f -o -name "*.pdf" ! -name '*-ocr.pdf' -print | while read file; do
   if ! [ -f "${file%.*}-ocr.pdf" ]; then
-    ocrmypdf -l deu --force-ocr --jbig2-lossy "${file}" "${file%.*}-ocr.pdf"
+    ocrmypdf -l deu+eng -c --jbig2-lossy --tesseract-timeout 60 "${file}" "${file%.*}-ocr.pdf"
     sleep 1
   else
     echo "Datei wurde bereits verarbeitet"
