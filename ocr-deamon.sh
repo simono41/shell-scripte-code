d697deda704e14eb0418b3ff021f33bfe6801d37
Simon Rieger <srieger@spectreos.de>
2022-02-15 08:52:01 +0100

    add timeout

--- ocr-deamon.sh
+++ ocr-deamon.sh
@@ -4,7 +4,7 @@ DOKUMENT_DIR=/home/simono41/data1/Dokumente
 
 find ${DOKUMENT_DIR} -path ${DOKUMENT_DIR}/.stversions -prune -type f -o -name "*.pdf" ! -name '*-ocr.pdf' -print | while read file; do
   if ! [ -f "${file%.*}-ocr.pdf" ]; then
-    ocrmypdf -l deu+eng -c --jbig2-lossy --tesseract-timeout 60 "${file}" "${file%.*}-ocr.pdf"
+    timeout 60 ocrmypdf -l deu+eng -c "${file}" "${file%.*}-ocr.pdf"
     sleep 1
   else
     echo "Datei wurde bereits verarbeitet"
